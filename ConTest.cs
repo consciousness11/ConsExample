626dba44d4aa084892f8886baea7771d5095e015
Marut <marut.dhungana@gmail.com>
2021-04-29 13:50:39 +0545

    Add project files.

---
+++ ConTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsExample
+{
+   public  class ConTest
+    {
+        //Constructor
+        public ConTest()
+        {
+            Console.WriteLine("Object Created");
+        }
+
+
+        ~ ConTest()
+        {
+            Console.WriteLine("Object Destroyed");
+        }
+
+    }
+}
